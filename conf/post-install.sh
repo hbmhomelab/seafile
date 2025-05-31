cfc793b843563d289d721f4ef9538e21a4630b77
Hannah Morrison <139557138+hbmorrison@users.noreply.github.com>
2025-05-31 23:23:57 +0100

    Basic post-install config

---
+++ conf/post-install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Location of the configuration files.
+
+SEAFILE_DIR="/opt/seafile"
+STARTUP_DIR="${SEAFILE_DIR}/seafile-server-latest"
+SEAHUB_SETTINGS="${SEAFILE_DIR}/conf/seahub_settings.py"
+
+# List of settings that should be added to seahub_settings.py from the environment.
+
+SEAHUB_SETTINGS_VARS="CSRF_TRUSTED_ORIGINS EMAIL_HOST EMAIL_PORT EMAIL_USE_TLS \
+  EMAIL_HOST_USER EMAIL_HOST_PASSWORD DEFAULT_FROM_EMAIL SERVER_EMAIL"
+
+# Additional variables that can be derived from environment variables.
+
+SERVER_EMAIL="${DEFAULT_FROM_EMAIL}"
+CSRF_TRUSTED_ORIGINS="[\"https://${SEAFILE_SERVER_HOSTNAME}\"]"
+
+# Add or replace each setting in seahub_settings.py.
+
+for VAR in $SEAHUB_SETTINGS_VARS
+do
+  if grep "^${VAR}" $SEAHUB_SETTINGS > /dev/null 2>&1
+  then
+    VALUE=`echo "${!VAR}" | sed 's|/|\\\/|g'`
+    sed -i -e "/^${VAR}\s*=/s/\s*=.*\$/ = ${VALUE}/" $SEAHUB_SETTINGS
+  else
+    echo "${VAR} = ${!VAR}" >> $SEAHUB_SETTINGS
+  fi
+done
+
+# Restart seafile server and seahub.
+
+$STARTUP_DIR/seafile.sh restart
+$STARTUP_DIR/seahub.sh restart
